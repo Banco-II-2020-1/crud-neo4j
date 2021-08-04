63d28485a5f38c02b05a12b49966a4464fc6d7ae
Paulo Freitas <paulo.freitas.nt@gmail.com>
2021-08-04 10:56:10 -0300

    Utilizando os counters e realizando ajustes nos exemplos.

--- src/main/java/com/ifpb/view/App.java
+++ src/main/java/com/ifpb/view/App.java
@@ -4,6 +4,7 @@ import com.ifpb.model.Pessoa;
 import org.neo4j.driver.*;
 
 import java.time.LocalDate;
+import java.util.stream.Collectors;
 
 import static org.neo4j.driver.Values.parameters;
 
@@ -12,27 +13,37 @@ public class App {
     public static void main(String[] args) {
 
         Driver driver = GraphDatabase.driver("bolt://localhost:7687",
-                AuthTokens.basic("neo4j", "neo4j1"));
+                AuthTokens.basic("neo4j", "neo4j*"));
 
-//        Pessoa pessoa = new Pessoa("222.222.222-02", "Maria",
-//                LocalDate.of(1993,10,25));
+        Pessoa pessoa = new Pessoa("222.222.222-02", "Maria",
+                LocalDate.of(1993,10,25));
 
         try(Session session = driver.session()){
 
             //Adicionando uma pessoa
-//            session.run("CREATE (p:Pessoa{cpf:$cpf, nome:$nome, nascimento:$nascimento})",
+//            Result result = session.run("CREATE (p:Pessoa{cpf:$cpf, nome:$nome, nascimento:$nascimento})",
 //                    parameters("cpf", pessoa.getCpf(), "nome", pessoa.getNome(),
 //                            "nascimento", pessoa.getNascimento()));
+//            System.out.println(result.consume().counters().nodesCreated());
 
             //Criar um relacionamento
-//            session.run("MATCH (p1:Pessoa{cpf:$cpf}),(p2:Pessoa{cpf:$cpf2})" +
+//            Result result = session.run("MATCH (p1:Pessoa{cpf:$cpf}),(p2:Pessoa{cpf:$cpf2})" +
 //                    "CREATE (p1)-[:AMIGO]->(p2)",
 //                    parameters("cpf", "111.111.111-01", "cpf2", "222.222.222-02"));
+//            System.out.println(result.consume().counters().relationshipsCreated());
 
-            Result result = session.run("MATCH (p:Pessoa{cpf:$cpf}) RETURN p",
-                    parameters("cpf", "111.111.111-01"));
+            //Recuperando todas as pessoas
+//            Result result = session.run("MATCH (p:Pessoa) RETURN p.cpf, p.nome, p.nascimento");
+//            System.out.println(result.stream().map(record ->
+//                    new Pessoa(record.get(0).asString(),
+//                    record.get(1).asString(),
+//                    record.get(2).asLocalDate()))
+//                    .collect(Collectors.toList()));
 
-            result.list().forEach(r -> System.out.println(r.get(0).asNode().values() ));
+            //Buscando os CPFs de todos os amigos de uma pessoa
+//            Result result = session.run("MATCH (p:Pessoa{cpf:$cpf})-[:AMIGO]->(p2) RETURN p2",
+//                    parameters("cpf", "111.111.111-01"));
+//            result.list().forEach(r -> System.out.println(r.get(0).asNode().values() ));
 
         }finally {
             driver.close();
